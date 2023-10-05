bf0e87a8e5a878c92414d4bb828411c8ed2994b2
tuxuser <462620+tuxuser@users.noreply.github.com>
2023-10-05 11:46:16 +0200

    Extend server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,19 +1,66 @@
-use std::net::Ipv4Addr;
-use anyhow::{Result, Context};
+use std::net::IpAddr;
+use anyhow::{anyhow, Result};
 use network_transfer::{Console, NetworkTransferProtocol};
+use network_interface::{NetworkInterface, NetworkInterfaceConfig};
 
 fn main() -> Result<()> {
     let protocol = NetworkTransferProtocol {};
 
+    let network_interfaces = NetworkInterface::show().unwrap();
+
+    let network_interfaces: Vec<NetworkInterface> = network_interfaces
+        .into_iter()
+        .filter(|intf|
+            intf.addr.iter().any(|&addr|{ addr.ip().is_ipv4() && !addr.ip().is_loopback()})
+        )
+        .collect();
+    
+    if network_interfaces.is_empty() {
+        return Err(anyhow!("No network interfaces enumerated, exiting"));
+    }
+
+    let bind_host = loop {
+        for (idx, intf) in network_interfaces.iter().enumerate() {
+            println!("{idx}) {} ({:?})", intf.name, intf.addr)
+        }
+    
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input) 
+            .expect("Failed to read line");
+        let choice: usize = input
+            .trim()
+            .parse()
+            .expect("Input not an integer");
+    
+        if choice > (network_interfaces.len() - 1) {
+            eprintln!("Invalid choice: {choice}, maximum interfaces: {}", network_interfaces.len() - 1);
+            continue;
+        }
+
+        let bind_result = network_interfaces.get(choice).unwrap().addr.iter().find_map(|addr| {
+            match addr.ip() {
+                IpAddr::V4(ip4_addr) => Some(ip4_addr),
+                _ => None,
+            }
+        });
+    
+        break bind_result.unwrap();
+    };
+
+    println!("Binding server to host: {bind_host:?}");
+
     let console = Console {
-        address: Ipv4Addr::new(10,0,0,229),
+        address: bind_host,
         port: 1248,
         id: "X1234".into(),
         name: "XBOXTEST".into(),
     };
 
-    let _ = protocol.announce(&console)
-        .context("Failed announcing network-transfer service")?;
+    std::thread::spawn(move || {
+        protocol.announce(&console)
+            .expect("Failed announcing network-transfer service");
+    });
 
     Ok(())
 }
